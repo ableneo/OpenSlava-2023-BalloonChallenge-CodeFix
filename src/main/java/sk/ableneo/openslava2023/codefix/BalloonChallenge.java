777354428b62c413cdb8aa2f3c8f22737d5334b1
Pawel Kruszewski OpenSlava 2023 <pawel.kruszewski@ableneo.com>
2023-10-19 04:24:53 -0700

    10

--- src/main/java/sk/ableneo/openslava2023/codefix/BalloonChallenge.java
+++ src/main/java/sk/ableneo/openslava2023/codefix/BalloonChallenge.java
@@ -11,21 +11,25 @@ import java.util.LinkedList;
 public class BalloonChallenge {
 
     public LinkedList findOrdersForProduct(Product p, boolean debug) {
-        ArrayList l = new ArrayList();
-        ArrayList list = getAllOrders();
-        for (int i = 0; i < list.size(); i++) {
-            Order order = (Order) list.get(i);
+        ArrayList checkedOrderList = new ArrayList();
+        ArrayList allOrderslist = getAllOrders();
+        for (Object o : allOrderslist) {
+            Order order = (Order) o;
             boolean found = false;
-            if (order.getProducts().size() > 0) {
-                for (int j = 0; j <= order.getProducts().size(); j++) {
-                    Product p2 = (Product) order.getProducts().get(j);
-                    found = (p2 == p);
+            if (!order.getProducts().isEmpty()) {
+                for (int j = 0; j < order.getProducts().size(); j++) {
+                    Product p2 = order.getProducts().get(j);
+                    found = (p2.equals(p));
                 }
-                if (found && order != null)
-                    l.add(order);
+                if (found)
+                    try {
+                        checkedOrderList.add(order);
+                    } catch (RuntimeException e) {
+                        throw new RuntimeException("Can`t add order " + e);
+                    }
             }
         }
-        return new LinkedList(l);
+        return new LinkedList(checkedOrderList);
     }
 
     public ArrayList getAllOrders() {
